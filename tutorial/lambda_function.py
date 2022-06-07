3f671e0d1d6e842f5cd060d1524eab9c7a8f3228
shalant <doug.rosenberg@gmail.com>
2022-06-07 10:36:22 -0500

    added CRUD code to lambda_function.py

--- tutorial/lambda_function.py
+++ tutorial/lambda_function.py
@@ -1,4 +1,3 @@
-import http
 import boto3
 import json
 from custom_encoder import CustomEncoder
@@ -38,8 +37,88 @@ def lambda_handler(event, context):
         response = deleteProduct(requestBody['productId'])
     else:
         response = buildResponse(404, 'Not Found')
-
     return response
+
+def getProduct(productId):
+    try:
+        response = table.get_item(
+            Key={
+                'productId': productId
+            }
+        )
+        if 'Item' in response:
+            return buildResponse(200, response['Item'])
+        else: 
+            return buildResponse(404, {'Message': 'ProductId: %s not found' % productId})
+    except:
+        logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+
+def getProducts():
+    try:
+        response = table.scan()
+        result = resonse['Item']
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            result.extend(response['Item'])
+
+        body = {
+            'products': repsonse
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+
+def saveProduct(requestBody):
+    try:
+        table.put_item(Item=requestBody)
+        body = {
+            'Operation': 'SAVE',
+            'Message': 'SUCCESS',
+            'Item': requestBody
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+
+def modifyProduct(productId, updateKey, updateValue):
+    try:
+        response = table.update_item(
+            Key={
+                'productId': productId
+            },
+            UpdateExpression='set %s = :value' % updateKey,
+            ExpressionAttributeValues={
+                ':value': updateValue
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        body = {
+            'Operation': 'UPDATE',
+            'Message': 'SUCCESS',
+            'UpdatedAttributes': response
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+
+def deleteProduct(productId):
+    try:
+        response = table.detele_item(
+            Key={
+                'productId': productId
+            },
+            ReturnValues='ALL_OLD'
+        )
+        body = {
+            'Operation': 'DELETE',
+            'Message': 'SUCCESS',
+            'deletedItem': response
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+
 
 def buildResponse(statusCode, body=None):
     response = {
